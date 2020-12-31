b496904776b0c09f5bcb1f65e4430e1893391f5a
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2020-12-31 10:55:23 +0700

    Add files via upload

---
+++ myreservationnew/home.js
@@ -0,0 +1,11 @@
+//selectors
+let header=document.querySelector('.header');
+let hamburgerMenu=document.querySelector('.hamburger-menu');
+
+window.addEventListener('scrol',function(){
+ let windowPosition = window.scrollY > 0;
+ header.classList.toggle('active', windowPosition)
+})
+hamburgerMenu.addEventListener('click',function () {
+    header.classList.toggle('menu-open');
+})
